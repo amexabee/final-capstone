087a1e84d2fc9678ace7b5195b39188504aaa32a
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-04-06 06:58:50 +0430

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_155900) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_010328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.bigint "user_id", null: false
+    t.bigint "swim_class_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["swim_class_id"], name: "index_bookings_on_swim_class_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "swim_classes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.string "image"
+    t.decimal "fee"
+    t.boolean "booked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_155900) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "swim_classes"
+  add_foreign_key "bookings", "users"
 end
