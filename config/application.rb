cdb5efcefcfe01ecc809825e333a8262ab779d61
Amanuel Galema <amexabee@yahoo.com>
2023-04-14 15:28:03 +0300

    Add lock file and update cors policy

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module FinalCapstone
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3009'
+        origins 'https://swimming.onrender.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
       end
     end
